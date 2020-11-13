4e3f777e2cb07c68e6deea3bfb3c6f3905c95ecf
Zatrac <eliasf_96@hotmail.com>
2020-11-13 05:19:06 +0000

    Enum type enforcing

--- ADS1256_DRIVER.h
+++ ADS1256_DRIVER.h
@@ -15,17 +15,17 @@
 * https://www.ti.com/lit/ds/symlink/ads1256.pdf (Page 30)
 */
 typedef enum {
-	REG_STATUS	= 0x00,		// b(ID3, ID2, ID1, ID0, ORDER, ACAL, BUFEN, DRDY)
-	REG_MUX		= 0x01,		// b(PSEL3, PSEL2, PSEL1, PSEL0, NSEL3, NSEL2, NSEL1, NSEL0)
-	REG_ADCON	= 0x02,		// b(0, CLK1, CLK0, SDCS1, SDCS0, PGA2, PGA1, PGA0)
-	REG_DRATE	= 0x03,		// b(DR7, DR6, DR5, DR4, DR3, DR2, DR1, DR0)
-	REG_IO		= 0x04,		// b(DIR3, DIR2, DIR1, DIR0, DIO3, DIO2, DIO1, DIO0)
-	REG_OFC0	= 0x05,		// b(OFC07, OFC06, OFC05, OFC04, OFC03, OFC02, OFC01, OFC00)
-	REG_OFC1	= 0x06,		// b(OFC15, OFC14, OFC13, OFC12, OFC11, OFC10, OFC09, OFC08)
-	REG_OFC2	= 0x07,		// b(OFC23, OFC22, OFC21, OFC20, OFC19, OFC18, OFC17, OFC16)
-	REG_FSC0	= 0x08,		// b(FSC07, FSC06, FSC05, FSC04, FSC03, FSC02, FSC01, FSC00)
-	REG_FSC1	= 0x09,		// b(FSC15, FSC14, FSC13, FSC12, FSC11, FSC10, FSC09, FSC08)
-	REG_FSC2	= 0x0A,		// b(FSC23, FSC22, FSC21, FSC20, FSC19, FSC18, FSC17, FSC16)
+	REG_STATUS			= 0x00,		// b(ID3, ID2, ID1, ID0, ORDER, ACAL, BUFEN, DRDY)
+	REG_MUX				= 0x01,		// b(PSEL3, PSEL2, PSEL1, PSEL0, NSEL3, NSEL2, NSEL1, NSEL0)
+	REG_ADCON			= 0x02,		// b(0, CLK1, CLK0, SDCS1, SDCS0, PGA2, PGA1, PGA0)
+	REG_DRATE			= 0x03,		// b(DR7, DR6, DR5, DR4, DR3, DR2, DR1, DR0)
+	REG_IO				= 0x04,		// b(DIR3, DIR2, DIR1, DIR0, DIO3, DIO2, DIO1, DIO0)
+	REG_OFC0			= 0x05,		// b(OFC07, OFC06, OFC05, OFC04, OFC03, OFC02, OFC01, OFC00)
+	REG_OFC1			= 0x06,		// b(OFC15, OFC14, OFC13, OFC12, OFC11, OFC10, OFC09, OFC08)
+	REG_OFC2			= 0x07,		// b(OFC23, OFC22, OFC21, OFC20, OFC19, OFC18, OFC17, OFC16)
+	REG_FSC0			= 0x08,		// b(FSC07, FSC06, FSC05, FSC04, FSC03, FSC02, FSC01, FSC00)
+	REG_FSC1			= 0x09,		// b(FSC15, FSC14, FSC13, FSC12, FSC11, FSC10, FSC09, FSC08)
+	REG_FSC2			= 0x0A,		// b(FSC23, FSC22, FSC21, FSC20, FSC19, FSC18, FSC17, FSC16)
 } ADS1256_REGISTERS;
 
 
@@ -34,21 +34,21 @@ typedef enum {
 * https://www.ti.com/lit/ds/symlink/ads1256.pdf (Page 34)
 */
 typedef enum {
-	CMD_WAKEUP		= 0x00,		// Completes SYNC and exits standby mode
-	CMD_RDATA		= 0x01,		// Read Data
-	CMD_RDATAC		= 0x03,		// Read Data Continuously
-	CMD_SDATAC		= 0x0F,		// Stop Read Data Continuously
-	CMD_RREG		= 0x10,		// Read from REG rrrr
-	CMD_WREG		= 0x50,		// Write from REG rrrr
-	CMD_SELFCAL		= 0xF0,		// Offset and Gain Self-Calibration
-	CMD_SELFOCAL	= 0xF1,		// Offset Self-Calibration
-	CMD_SELFGCAL	= 0xF2,		// Gain Self-Calibration
-	CMD_SYSOCAL		= 0xF3,		// System Offset Calibration
-	CMD_SYSGCAL		= 0xF4,		// System Gain Calibration
-	CMD_SYNC		= 0xFC,		// Synchronize the A/D Conversion
-	CMD_STANDBY		= 0xFD,		// Begin Standby Mode
-	CMD_RESET		= 0xFE,		// Reset to Power-Up Values
-	CMD_WAKEUP_1	= 0xFF,		// Completes SYNC and exits standby mode
+	CMD_WAKEUP			= 0x00,		// Completes SYNC and exits standby mode
+	CMD_RDATA			= 0x01,		// Read Data
+	CMD_RDATAC			= 0x03,		// Read Data Continuously
+	CMD_SDATAC			= 0x0F,		// Stop Read Data Continuously
+	CMD_RREG			= 0x10,		// Read from REG rrrr
+	CMD_WREG			= 0x50,		// Write from REG rrrr
+	CMD_SELFCAL			= 0xF0,		// Offset and Gain Self-Calibration
+	CMD_SELFOCAL		= 0xF1,		// Offset Self-Calibration
+	CMD_SELFGCAL		= 0xF2,		// Gain Self-Calibration
+	CMD_SYSOCAL			= 0xF3,		// System Offset Calibration
+	CMD_SYSGCAL			= 0xF4,		// System Gain Calibration
+	CMD_SYNC			= 0xFC,		// Synchronize the A/D Conversion
+	CMD_STANDBY			= 0xFD,		// Begin Standby Mode
+	CMD_RESET			= 0xFE,		// Reset to Power-Up Values
+	CMD_WAKEUP_1		= 0xFF,		// Completes SYNC and exits standby mode
 } ADS1256_COMMANDS;
 
 
@@ -135,9 +135,9 @@ static bool ADS1256_DRDY() {
 * 
 * Pulls the CS pin low then sends the command and raises the CS pin high
 * 
-* @param CMD. The command defined in the ADS1256_COMMANDS enum
+* @param ADS1256_COMMANDS The command defined in the ADS1256_COMMANDS enum
 */
-static void ADS1256_SENDCMD(uint8_t CMD) {
+static void ADS1256_SENDCMD(ADS1256_COMMANDS CMD) {
 	bcm2835_gpio_write(ADS_CS_PIN, 0);
 	bcm2835_spi_transfer(CMD);
 	bcm2835_gpio_write(ADS_CS_PIN, 1);
@@ -150,10 +150,10 @@ static void ADS1256_SENDCMD(uint8_t CMD) {
 * Pulls the CS pin low then sends the WREG command 
 * followed by the data and then raises the CS pin high
 *
-* @param uint8_t REG. The register defined in the ADS1256_REGISTERS enum
-* @param uint8_t DATA. The value to set the register
+* @param ADS1256_REGISTERS The register defined in the ADS1256_REGISTERS enum
+* @param uint8_t The value to set the register
 */
-static void ADS1256_WREG(uint8_t REG, uint8_t DATA) {
+static void ADS1256_WREG(ADS1256_REGISTERS REG, uint8_t DATA) {
 	bcm2835_gpio_write(ADS_CS_PIN, 0);
 
 	bcm2835_spi_transfer(CMD_WREG | REG);
@@ -169,10 +169,10 @@ static void ADS1256_WREG(uint8_t REG, uint8_t DATA) {
 * Pulls the CS pin low then sends the WREG command
 * followed by the data and then raises the CS pin high
 *
-* @param uint8_t REG. The register defined in the ADS1256_REGISTERS enum
+* @param ADS1256_REGISTERS REG. The register defined in the ADS1256_REGISTERS enum
 * @return uint8_t The value from REG
 */
-static uint8_t ADS1256_RREG(uint8_t REG) {
+static uint8_t ADS1256_RREG(ADS1256_REGISTERS REG) {
 	uint8_t value = 0;
 
 	bcm2835_gpio_write(ADS_CS_PIN, 0);
@@ -190,9 +190,9 @@ static uint8_t ADS1256_RREG(uint8_t REG) {
 
 /**
 * Set the channel to be read
-* @param uint8_t A channel as defined in the ADS1256_CHANNELS enum
+* @param ADS1256_CHANNELS A channel as defined in the ADS1256_CHANNELS enum
 */
-static void ADS1256_SETCHANNEL(uint8_t CHANNEL) {
+static void ADS1256_SETCHANNEL(ADS1256_CHANNELS CHANNEL) {
 	ADS1256_WREG(REG_MUX, (CHANNEL << 4) | (1 << 3));
 }
 
